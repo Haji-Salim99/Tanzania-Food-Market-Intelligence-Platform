3de85ab58aac2075026d9e0beb82b197a8298df4
Haji <hajisalim029@gmail.com>
2026-05-20 01:16:12 +0300

    Initialize project structure, dependencies, environment config, and git hygiene

---
+++ config/constants.py
@@ -0,0 +1,5 @@
+FOOD_PRICES_ENDPOINT = "/food-security-nutrition-poverty/food-prices-market-monitor"
+
+DEFAULT_PAGE_SIZE = 1000
+
+OUTPUT_FORMAT = "json"
---
+++ config/settings.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HDX_APP_IDENTIFIER = os.getenv("HDX_APP_IDENTIFIER")
+HDX_BASE_URL = os.getenv("HDX_BASE_URL")
+GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
+GCP_DATASET= os.getenv("GCP_DATASET")
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+COUNTRY_CODE = os.getenv("COUNTRY_CODE")
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
